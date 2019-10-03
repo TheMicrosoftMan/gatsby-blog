a7019b9aacd302801c9b004e9e2b06887c1bc939
TheMicrosoftMan <yurazelenko@windowslive.com>
2019-10-03 17:39:26 +0300

    Add user screen on GitHub page and some designed for repo screen

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -7,34 +7,79 @@ import GitHubRepo from "../components/GitHubRepo"
 import Layout from "../components/Layout/layout"
 import SEO from "../components/SEO/seo"
 
-const languagesConstnts = {
-  JAVASCRIPT: "JavaScript",
-  CSS: "CSS",
-  HTML: "HTML",
-  CSharp: "C#",
+const colorsArr = [
+  "#FFB900",
+  "#F7630C",
+  "#EF6950",
+  "#D13438",
+  "#C30052",
+  "#C239B3",
+  "#9A0089",
+  "#881798",
+  "#744DA9",
+  "#8764B8",
+  "#0063B1",
+  "#0099BC",
+  "#038387",
+  "#00B294",
+  "#00CC6A",
+  "#10893E",
+  "#68768A",
+  "#567C73",
+  "#847545",
+]
+
+const screens = {
+  USER: "USER",
+  REPO: "REPO",
 }
 
 const ReposPage = () => {
-  const [githubRepos, getGithubRepos] = useState([])
+  const [gitHubUserInfo, setGitHubUserInfo] = useState({})
+  const [githubRepos, setGithubRepos] = useState([])
   const [activeRepo, setActiveRepo] = useState({})
 
+  const [activeScreen, setActiveScreen] = useState(screens.USER)
+
   useEffect(() => {
-    axios
-      .get("https://api.github.com/users/themicrosoftman/repos?sort=updated")
-      .then(data => {
-        getGithubRepos(
-          data.data.map(repo => {
-            return {
-              id: repo.id,
-              name: repo.name,
-              description: repo.description,
-              html_url: repo.html_url,
-            }
-          })
-        )
+    async function loadData() {
+      const gitHubUserInfoJSON = await getGitHubUserInfo()
+      setGitHubUserInfo({
+        login: gitHubUserInfoJSON.data.login,
+        avatar_url: gitHubUserInfoJSON.data.avatar_url,
+        html_url: gitHubUserInfoJSON.data.html_url,
+        name: gitHubUserInfoJSON.data.name,
+        location: gitHubUserInfoJSON.data.location,
+        public_repos: gitHubUserInfoJSON.data.public_repos,
+        created_at: gitHubUserInfoJSON.data.created_at,
+        updated_at: gitHubUserInfoJSON.data.updated_at,
       })
-      .catch(err => console.log(err))
+
+      const githubReposJSON = await getGithubRepos()
+      setGithubRepos(
+        githubReposJSON.data.map(repo => {
+          return {
+            id: repo.id,
+            name: repo.name,
+            description: repo.description,
+            html_url: repo.html_url,
+          }
+        })
+      )
+    }
+
+    loadData()
   }, [])
+
+  const getGitHubUserInfo = () => {
+    return axios.get("https://api.github.com/users/themicrosoftman")
+  }
+
+  const getGithubRepos = () => {
+    return axios.get(
+      "https://api.github.com/users/themicrosoftman/repos?sort=updated"
+    )
+  }
 
   const getRepoInfo = repo_id => {
     axios
@@ -78,7 +123,10 @@ const ReposPage = () => {
               date: commit.commit.author.date,
             }
           }),
+          colors: getRandomColors(reposLanguagesDetailArr.length),
         })
+
+        setActiveScreen(screens.REPO)
       })
       .catch(err => console.log(err))
   }
@@ -95,11 +143,23 @@ const ReposPage = () => {
     )
   }
 
+  const getRandomColors = count => {
+    let colorsSet = new Set()
+    while (colorsSet.size < count) {
+      const randomColor = colorsArr[getRandomValue(0, count)]
+      colorsSet.add(randomColor)
+    }
+    return [...colorsSet]
+  }
+
+  const getRandomValue = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
   return (
     <Layout>
       <SEO title="Repos" />
       <div className="page github">
-        {/* <h2>GitHub repositories</h2> */}
         <div className="PostsList">
           {githubRepos &&
             githubRepos.map((repo, index) => {
@@ -115,109 +175,192 @@ const ReposPage = () => {
               )
             })}
         </div>
-
-        {activeRepo.hasOwnProperty("full_name") && (
-          <div className="repo_info">
-            <a
-              href={activeRepo.html_url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="repo_info_name"
-            >
-              {activeRepo.full_name}
-            </a>
-            <div className="repo_info_text">
-              <p className="repo_info_text_desc">{activeRepo.description}</p>
-            </div>
-            <div className="repo_info_languages">
-              <details>
-                <summary>
-                  <span className="repo_info_languages_title">
-                    Languages list
-                  </span>
-                </summary>
-                <ul className="repo_info_languages-list">
-                  {activeRepo.reposLanguages.map((lang, index) => {
-                    return (
-                      <li key={index} className="repo_info_languages-list_item">
-                        <span className="repo_info_languages-list_item_name">
-                          {lang.name}:
-                        </span>
-                        <span className="repo_info_languages-list_item_size">
-                          {lang.bytes} bytes
-                        </span>
-                        -
-                        <span className="repo_info_languages-list_item_percent">
-                          {lang.percent}%
-                        </span>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </details>
-            </div>
-            <div className="repo_info_commits">
-              <details>
-                <summary>
-                  <span className="repo_info_commits_title">Commits list</span>
-                </summary>
-                <ul className="repo_info_commits-list">
-                  {activeRepo.reposCommits.map((commit, index) => {
-                    return (
-                      <li key={index} className="repo_info_commits-list_item">
-                        <span className="repo_info_commits-list_item_date">
-                          {moment(commit.date).format("DD.MM.YYYY HH:mm:ss")}
-                        </span>
-                        :
-                        <span className="repo_info_commits-list_item_msg">
-                          {commit.message}
-                        </span>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </details>
-            </div>
-            <div className="repo_info_chart">
-              <Pie
-                data={{
-                  labels: activeRepo.reposLanguages.map(lang => {
-                    return lang.name
-                  }),
-                  datasets: [
-                    {
-                      data: activeRepo.reposLanguages.map(lang => {
-                        return lang.percent
-                      }),
-                      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-                    },
-                  ],
+        {activeScreen === screens.REPO &&
+          activeRepo.hasOwnProperty("full_name") && (
+            <div className="repo_info">
+              <div
+                className="repo_info_close"
+                onClick={() => {
+                  setActiveScreen(screens.USER)
                 }}
-                options={{ legend: { position: "bottom" } }}
-              />
+              >
+                <i className="mi mi-Clear"></i>
+              </div>
+              <div className="repo_info_head">
+                <img
+                  src={gitHubUserInfo.avatar_url}
+                  className="repo_info_head_avatar"
+                />
+                <a
+                  href={activeRepo.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="repo_info_name"
+                >
+                  {activeRepo.full_name}
+                </a>
+              </div>
+              <div className="repo_info_text">
+                <p className="repo_info_text_desc">{activeRepo.description}</p>
+              </div>
+              <div className="repo_info_languages-commits-chart">
+                <div className="repo_info_languages-commits">
+                  <div className="repo_info_text_at">
+                    <div className="repo_info_text">
+                      <span className="repo_info_text_label">Created at: </span>
+                      <span className="repo_info_text_value">
+                        {moment(activeRepo.created_at).format(
+                          "DD.MM.YYYY HH:mm:ss"
+                        )}
+                      </span>
+                    </div>
+                    <div className="repo_info_text">
+                      <span className="repo_info_text_label">Updated at: </span>
+                      <span className="repo_info_text_value">
+                        {moment(activeRepo.updated_at).format(
+                          "DD.MM.YYYY HH:mm:ss"
+                        )}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="repo_info_languages">
+                    <details open>
+                      <summary>
+                        <span className="repo_info_languages_title">
+                          Languages list
+                        </span>
+                      </summary>
+                      <ul className="repo_info_languages-list">
+                        {activeRepo.reposLanguages.map((lang, index) => {
+                          return (
+                            <li
+                              key={index}
+                              className="repo_info_languages-list_item"
+                            >
+                              <span className="repo_info_languages-list_item_name">
+                                {lang.name}:
+                              </span>
+                              <span className="repo_info_languages-list_item_size">
+                                {lang.bytes} bytes
+                              </span>
+                              -
+                              <span className="repo_info_languages-list_item_percent">
+                                {lang.percent}%
+                              </span>
+                            </li>
+                          )
+                        })}
+                      </ul>
+                    </details>
+                  </div>
+                  <div className="repo_info_commits">
+                    <details open>
+                      <summary>
+                        <span className="repo_info_commits_title">
+                          Commits list
+                        </span>
+                      </summary>
+                      <ul className="repo_info_commits-list">
+                        {activeRepo.reposCommits.map((commit, index) => {
+                          return (
+                            <li
+                              key={index}
+                              className="repo_info_commits-list_item"
+                            >
+                              <span className="repo_info_commits-list_item_date">
+                                {moment(commit.date).format(
+                                  "DD.MM.YYYY HH:mm:ss"
+                                )}
+                              </span>
+                              :
+                              <span className="repo_info_commits-list_item_msg">
+                                {commit.message}
+                              </span>
+                            </li>
+                          )
+                        })}
+                      </ul>
+                    </details>
+                  </div>
+                </div>
+                <div className="repo_info_chart">
+                  <Pie
+                    data={{
+                      labels: activeRepo.reposLanguages.map(lang => {
+                        return lang.name
+                      }),
+                      datasets: [
+                        {
+                          data: activeRepo.reposLanguages.map(lang => {
+                            return lang.percent
+                          }),
+                          backgroundColor: activeRepo.colors,
+                          hoverBackgroundColor: activeRepo.colors,
+                        },
+                      ],
+                    }}
+                    options={{ legend: { position: "bottom" } }}
+                  />
+                </div>
+              </div>
             </div>
-            <div className="repo_info_text">
-              <span className="repo_info_text_label">Created at: </span>
-              <span className="repo_info_text_value">
-                {moment(activeRepo.created_at).format("DD.MM.YYYY HH:mm:ss")}
-              </span>
-            </div>
-            <div className="repo_info_text">
-              <span className="repo_info_text_label">Updated at: </span>
-              <span className="repo_info_text_value">
-                {moment(activeRepo.updated_at).format("DD.MM.YYYY HH:mm:ss")}
-              </span>
-            </div>
-          </div>
-        )}
-        {githubRepos &&
-          githubRepos.length > 0 &&
-          !activeRepo.hasOwnProperty("full_name") && (
-            <div className="repos">
-              {githubRepos.map((repo, index) => {
-                return <GitHubRepo key={index} id={index + 1} repo={repo} />
-              })}
+          )}
+        {activeScreen === screens.USER &&
+          gitHubUserInfo.hasOwnProperty("login") && (
+            <div className="user_info">
+              <div className="info">
+                <img src={gitHubUserInfo.avatar_url} className="info_avatar" />
+                <div className="info_user">
+                  <a
+                    href={gitHubUserInfo.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="info_user_login repo_info_name"
+                  >
+                    {gitHubUserInfo.login}
+                  </a>
+                  <span className="info_user_name">{gitHubUserInfo.name}</span>
+                  <span className="info_user_location">
+                    <i className="mi mi-Location"></i>
+                    {gitHubUserInfo.location}
+                  </span>
+                  <span className="info_user_at">
+                    <i className="mi mi-EmojiTabCelebrationObjects"></i>
+                    <span className="info_user_at_date">
+                      {moment(gitHubUserInfo.created_at).format(
+                        "DD.MM.YYYY HH:mm:ss"
+                      )}
+                    </span>
+                    <span className="info_user_at_text">Joined to GitHub</span>
+                  </span>
+                  <span className="info_user_at">
+                    <i className="mi mi-DevUpdate"></i>
+                    <span className="info_user_at_date">
+                      {moment(gitHubUserInfo.updated_at).format(
+                        "DD.MM.YYYY HH:mm:ss"
+                      )}
+                    </span>
+                    <span className="info_user_at_text">Last update</span>
+                  </span>
+                </div>
+              </div>
+              {githubRepos && githubRepos.length > 0 && (
+                <div className="repos-block">
+                  <h3 className="repos_head">
+                    Public repositories{" "}
+                    <span className="repos_head_repo-count">
+                      {githubRepos.length}
+                    </span>
+                  </h3>
+                  <div className="repos">
+                    {githubRepos.map((repo, index) => {
+                      return (
+                        <GitHubRepo key={index} id={index + 1} repo={repo} />
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
             </div>
           )}
       </div>
